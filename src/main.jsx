b7e308ef86d2a9b30117032d14ae9166b92b92d7
dhemjitha <dhemjitha@gmail.com>
2024-08-06 20:18:35 +0530

    fixed: route

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
         element: <HomePage />,
       },
       {
-        path: "about",
+        path: "/about",
         element: <AboutPage />,
       },
     ],

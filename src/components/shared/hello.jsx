a7f587c57f5cd082d5f5589d205ef464832bf7db
dhemjitha <dhemjitha@gmail.com>
2024-10-20 18:23:10 +0530

    fix: Avatar updated

--- src/components/shared/hello.jsx
+++ src/components/shared/hello.jsx
@@ -5,7 +5,7 @@ function Hello() {
     <main>
       <div className="py-6">
         <Avatar>
-          <AvatarImage src="https://media.licdn.com/dms/image/D5603AQHNo8f5n--PvA/profile-displayphoto-shrink_800_800/0/1720128085923?e=1728518400&v=beta&t=K4Fcq0Q0ncNvPVbsIG2WEDBVr9c9jEG2i06I5zof8k0" />
+          <AvatarImage src="https://media.licdn.com/dms/image/v2/D5603AQHNo8f5n--PvA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1720128085923?e=1735171200&v=beta&t=flvEb17uYxmTNmJVPPL9xI8rU8_cS8F98cYCJ-JJl1U" />
         </Avatar>
       </div>
       <div>
